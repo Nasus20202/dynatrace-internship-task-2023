ca937c4bef2a1459573600e7db1fe907c305fe73
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-04-22 13:56:54 +0200

    navbar

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,10 +1,47 @@
 import {Outlet} from "react-router-dom";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {Link} from 'react-router-dom';
 
 function Layout() {
     return (
         <main>
-            Layout
-            <Outlet />
+            <Navbar bg="dark" variant="dark" expand="sm">
+                <Container>
+                    <Navbar.Brand>
+                        <Link to="/">
+                            Currency API
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbar"/>
+                    <Navbar.Collapse id="navbar">
+                        <Nav>
+                        <Nav.Link>
+                            <Link to="/">
+                                Average
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/extremes">
+                                Extremes
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/differences">
+                                Differences
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/codes">
+                                Codes
+                            </Link>
+                        </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Container>
+                <Outlet/>
+            </Container>
         </main>
     )
 }
